4ad599d6cf9b1708d87cb12d72b4bf4ae1764263
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-01-08 11:24:52 +0100

    add current date to navber

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 import Trending from "./trending.js";
 import LatestArticle from "./latest.js";
 import Modal from "./modal.js";
+import { getCurrentDate, setInnerHTML, getElement } from "./modules/index.js";
+const todayDate = getElement(".date");
 
 Trending();
 LatestArticle();
 Modal();
+
+setInnerHTML(todayDate, getCurrentDate());
+
+
