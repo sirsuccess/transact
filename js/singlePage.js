cb435a33a60d930da3dec1f215e224efdb0c1655
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-01-10 04:13:26 +0100

    update implemented

--- js/singlePage.js
+++ js/singlePage.js
@@ -10,7 +10,8 @@ import createNode, {
   asyncGetArticles,
   getStorage,
   DeleteFetch,
-  RedirectFuntion
+  RedirectFuntion,
+  EditFetch
   //   displayArticle
 } from "./modules/index.js";
 import Navbar from "./navbar.js";
@@ -20,6 +21,10 @@ import Modal from "./modal.js";
 const setImg = getElement("#sliderImg");
 const sliderText = getElement(".slider-text");
 const DeleteTrash = getElement(".fa-trash");
+const titleInput = getElement("#title");
+const urlInput = getElement("#url");
+const avatarInput = getElement("#avatar");
+const submitBtn = getElement(".submit-btn");
 
 //navbar toggle
 Navbar();
@@ -27,7 +32,8 @@ Navbar();
 //date output
 CurrentDate();
 
-/
+//toggle modal
+Modal();
 
 //get id from session storage
 let id = getStorage("articleID");
@@ -39,13 +45,45 @@ asyncGetArticles(
   .then(data => {
     setImageAttribute(setImg, data.avatar);
     setInnerHTML(sliderText, data.title);
+
+    //set the value of input
+    titleInput.value = data.title;
+    urlInput.value = data.url;
+    avatarInput.value = data.avatar;
+
+    //click event for POST
+    clickEvent(submitBtn, e => {
+      e.preventDefault();
+      let createdAt = Date.now();
+      let title = titleInput.value ? titleInput.value : data.title;
+      let url = urlInput.value ? urlInput.value : data.url;
+      const avatar = avatarInput.value ? avatarInput.value : data.avatar;
+
+      const body = {
+        createdAt,
+        title,
+        url,
+        avatar
+      };
+      EditFetch(
+        `http://5e0df4b536b80000143db9ca.mockapi.io/etranzact/v1/article/${id}`,
+        body
+      )
+        .then(data => {
+          alert("Upate successful");
+          return RedirectFuntion("./single-article.html");
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
   })
   .catch(err => {
     console.log(err);
   });
 
-  //click event for delete 
-clickEvent(DeleteTrash, ()=>{
+//click event for delete
+clickEvent(DeleteTrash, () => {
   if (confirm("Do you want to Delete this Article")) {
     DeleteFetch(
       `http://5e0df4b536b80000143db9ca.mockapi.io/etranzact/v1/article/${id}`
@@ -61,5 +99,3 @@ clickEvent(DeleteTrash, ()=>{
     console.log("cancel request");
   }
 });
-
-
