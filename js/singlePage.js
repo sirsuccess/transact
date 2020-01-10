5d84d7da1790027b1ad17afc09a25f9cff65be94
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-01-10 02:13:20 +0100

    delete item implemented

--- js/singlePage.js
+++ js/singlePage.js
@@ -8,23 +8,31 @@ import createNode, {
   DisplayBlock,
   getCurrentDate,
   asyncGetArticles,
-  getStorage
+  getStorage,
+  DeleteFetch,
+  RedirectFuntion
   //   displayArticle
 } from "./modules/index.js";
+import Navbar from "./navbar.js";
+import CurrentDate from "./date.js";
+import Modal from "./modal.js";
 
-// global variable
-const date = getElement(".date");
 const setImg = getElement("#sliderImg");
 const sliderText = getElement(".slider-text");
+const DeleteTrash = getElement(".fa-trash");
 
-
+//navbar toggle
+Navbar();
 
 //date output
-setInnerHTML(date, getCurrentDate());
+CurrentDate();
+
+/
 
 //get id from session storage
 let id = getStorage("articleID");
 
+//get fetch item
 asyncGetArticles(
   `http://5e0df4b536b80000143db9ca.mockapi.io/etranzact/v1/article/${id}`
 )
@@ -35,3 +43,23 @@ asyncGetArticles(
   .catch(err => {
     console.log(err);
   });
+
+  //click event for delete 
+clickEvent(DeleteTrash, ()=>{
+  if (confirm("Do you want to Delete this Article")) {
+    DeleteFetch(
+      `http://5e0df4b536b80000143db9ca.mockapi.io/etranzact/v1/article/${id}`
+    )
+      .then(data => {
+        alert("Delete successful");
+        return RedirectFuntion("./index.html");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  } else {
+    console.log("cancel request");
+  }
+});
+
+
