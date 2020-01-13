7e7a9ade33aeafb524430205b513fc052d49af9f
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-01-13 12:21:36 +0100

    remove double naming of function

--- js/registerSW.js
+++ js/registerSW.js
@@ -1,11 +1,9 @@
-export default registerSW = () => {
-  async function registerSW() {
-    if ("serviceWorker" in navigator) {
-      try {
-        await navigator.serviceWorker.register("./sw.js");
-      } catch (e) {
-        console.log(`SW registration failed`);
-      }
+export default async function registerSW() {
+  if ("serviceWorker" in navigator) {
+    try {
+      await navigator.serviceWorker.register("./sw.js");
+    } catch (e) {
+      console.log(`SW registration failed`);
     }
   }
-};
+}
